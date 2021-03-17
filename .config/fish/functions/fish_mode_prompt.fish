0cd615180132bb9a14b85c70ce1503e585b1df70
João Loureiro <loureiro.jpc@gmail.com>
2021-03-17 19:50:21 +0000

    fish: update vi mode prompt

---
+++ .config/fish/functions/fish_mode_prompt.fish
@@ -0,0 +1,25 @@
+function fish_mode_prompt --description "Display the default mode for the prompt"
+    # Do nothing if not in vi mode
+    if test "$fish_key_bindings" = "fish_vi_key_bindings"
+        or test "$fish_key_bindings" = "fish_hybrid_key_bindings"
+        switch $fish_bind_mode
+            case default
+                set_color --bold --background red black
+                echo '[N]'
+            case insert
+                set_color --bold --background green black
+                echo '[I]'
+            case replace_one
+                set_color --bold --background green black
+                echo '[R]'
+            case replace
+                set_color --bold --background cyan black
+                echo '[R]'
+            case visual
+                set_color --bold --background magenta black
+                echo '[V]'
+        end
+        set_color normal
+        echo -n ' '
+    end
+end
